88405f1786baa57b2f9753960a8d1a31fb506833
Cheng Nie <niechenghust@gmail.com>
2015-08-23 15:01:27 -0500

    Commit 3 with clear structure

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,11 @@
 #     makeCacheMatrix can create a special object that has both a matrix and its inverse in the environment
 #     cacheSolve can get the inverse of the matrix special 
 # Though it's not required. A test of performance is also included in the end
+#################################################################################
 
 # makeCacheMatrix function is used to create a special object that 
 # stores a matrix and caches its inverse
+#################################################################################
 
 makeCacheMatrix <- function(x = matrix()) {
     ## create a special object 
@@ -29,6 +31,7 @@ makeCacheMatrix <- function(x = matrix()) {
 # It first checks if the inverse has already been computed. If so, it gets the inverse
 # without the computation. If not, it computes the inverse and set the value of the inverse 
 # through setsolve function.
+#################################################################################
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
@@ -50,16 +53,21 @@ cacheSolve <- function(x, ...) {
 
 
 # Test submission's performance by retrieving inverse of some randomly generated matrix 
+#################################################################################
+
 # In this test set, I generate 5000 20 by 20 matrix with all 400 elements randomly drawn from 
 # a uniform distribution between 0 and 10000. 
 myObj <- list(makeCacheMatrix(matrix(runif(400,0,10000), 20, 20)))
 for(j in 1:4999) myObj <- append(myObj,list(makeCacheMatrix(matrix(runif(400,0,10000), 20, 20))))
+
 # first pass of getting the inverse
 # it takes 0.018 seconds on my system (Mac OSX with 16G RAM)
 system.time(for(item in myObj) { cacheSolve(item) }, gcFirst = TRUE ) 
+
 # second pass 
 # it takes 0.002 seconds on my system 
 system.time(for(item in myObj) { cacheSolve(item) }, gcFirst = TRUE ) 
+
 # First pass is slower than the second pass because each inverse is calculated from scratch in the first pass.
 # Second pass only retrive the inverse directly.
 
